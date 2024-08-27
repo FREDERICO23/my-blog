1b2e9950a65b85497741a9ba8b5ef98aa28fe8d0
FREDERICO23 <adhingafredrick@gmail.com>
2024-08-28 01:11:07 +0300

    chore: Add initial project files and configurations

---
+++ config/middlewares.js
@@ -0,0 +1,34 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://yourvueapp.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://yourvueapp.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:8080', 'https://yourvueapp.com'],
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
